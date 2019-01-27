b70583746141f4d09f61d3a29a2ece0f8e468741
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-01-27 14:45:17 +0000

    Added visibility on SMS

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const socketio = require('socket.io');
 
 // Init Nexmo
 const nexmo = new Nexmo({
-  apiKey: '',
-  apiSecret: ''
+  apiKey: '8196a2b6',
+  apiSecret: 'xO4syW8tIdwG605H'
 }, {debug: true});
 
 // Init app
@@ -43,6 +43,15 @@ app.post('/', (req, res) => {
         console.log(err);
       } else {
         console.dir(responseData);
+
+        // Get data from the response
+        const data = {
+          id: responseData.messages[0]['message-id'],
+          number: responseData.messages[0]['to']
+        }
+
+        // Emit to the client
+        io.emit('smsStatus', data);
       }
     }
   );
@@ -53,3 +62,12 @@ const port = 3000;
 
 // Start server
 const server = app.listen(port, () => console.log(`Server started on ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Connected');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+})
--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,11 @@ const numberInput = document.getElementById('number'),
 
 button.addEventListener('click', send, false);
 
+const socket = io();
+socket.on('smsStatus', function(data){
+  response.innerHTML = '<h5>Text message sent to ' + data.number + '</h5>';
+})
+
 function send() {
   const number = numberInput.value.replace(/\D/g, '');
   const text = textInput.value;
