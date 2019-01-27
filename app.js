aa243f1219fe0923a88eefc98821cdb00c882814
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-01-27 11:29:18 +0000

    Initialize app with express

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socket.io = require('socket.io');
+
+
+// Init app
+const app = express();
