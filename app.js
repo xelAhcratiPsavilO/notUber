fc12fbad0c82f9ccefe891bfa281d6e0142e42bd
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-01-27 11:27:48 +0000

    Added all dependencies to entry file

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socket.io = require('socket.io');
