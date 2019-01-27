1a30e319589c38291a6272b49ef1e066eb23a47d
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-01-27 11:42:21 +0000

    Defined port to start server

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ app.use(express.static(__dirname + '/public'));
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log('Server started on port ${port}'));
