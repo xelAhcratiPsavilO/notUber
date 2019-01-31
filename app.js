3a07b4d1f4e9d6d83ae1178cb2267e7c8ad2c939
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2019-01-31 00:02:51 +0000

    Edited PORT

--- app.js
+++ app.js
@@ -100,10 +100,10 @@ app.get('/response', (req, res) => {
 });
 
 // Define port
-const port = 3000;
+const PORT = process.env.PORT || 5000
 
 // Start server
-const server = app.listen(port, () => console.log(`Server started on ${port}`));
+const server = app.listen(PORT, () => console.log(`Server started on ${ PORT }`);
 
 // Connect to socket.io
 const io = socketio(server);
